7b23a14b2d9252626957959913edc75276b401e7
Vaibhav Shrikant Suryawanshi <suryawanshi.s.vaibhav@gmail.com>
2026-06-21 23:07:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+
+from bot import chat
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get", methods=["POST"])
+def get_bot_response():
+    user_text = request.form.get("msg", "")
+    response = chat(user_text)
+    return jsonify({"response": response})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
